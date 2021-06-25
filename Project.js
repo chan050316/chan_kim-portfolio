131fa1d4caf587070722eb9f66e5313c8035e9fb
chan050316 <84690440+chan050316@users.noreply.github.com>
2021-06-25 23:29:46 +0900

    F / C 2021.6.25.11:28(PM)
    add link to googledrive and instagram
    create project_info.css / project_info_single1

--- Project.js
+++ Project.js
@@ -3,30 +3,31 @@ var projects = [
         project_name: "개인주제 프로젝트",
         info_data: [
             {
+                href_1: "./project_info.html",
                 project_date1: "2020.3분기",
                 project_date2: "2020.4분기",
                 project_date3: ""
             }
-        ],
-        href: ""
+        ]
     },
 
     {
         project_name: "주제중심 프로젝트",
         info_data: [
             {
+                href_1: "",
                 project_date1: "2020_3분기",
                 project_date2: "2020_4분기",
                 project_date3: ""
             }
-        ],
-        href: ""
+        ]
     },
 
     {
         project_name: "문제정의 프로젝트",
         info_data: [
             {
+                href_1: "",
                 project_date1: "2020_3분기",
                 project_date2: "2020_4분기",
                 project_date3: "2021_1분기 ~ 2021_2분기"
@@ -55,7 +56,7 @@ for (var i in projects) {
         document.getElementById("contents_BBox").innerHTML += `
         <div id="contents_Box1">
             <div class="Btext">${project.project_name}</div>
-            <div class="Stext"><a href="${project.href}">${date.project_date1}</a></div>
+            <div class="Stext"><a href="${date.href_1}">${date.project_date1}</a></div>
             <div class="Stext"><a href="${project.href}">${date.project_date2}</a></div>
             <div class="Stext"><a href="${project.href}">${date.project_date3}</a></div>
         </div>
