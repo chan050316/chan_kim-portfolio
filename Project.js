cb78e6ecbdba634b30a92edca219b4a234e8e284
chan050316 <84690440+chan050316@users.noreply.github.com>
2021-06-28 12:29:09 +0900

    F 2021.6.28.12:28(PM)
    fixed 'project' page html

--- Project.js
+++ Project.js
@@ -5,7 +5,9 @@ var projects = [
             {
                 href_1: "./project_info_single1.html",
                 project_date1: "2020.3분기",
+                href_2: "",
                 project_date2: "2020.4분기",
+                href_3: "",
                 project_date3: ""
             }
         ]
@@ -17,7 +19,9 @@ var projects = [
             {
                 href_1: "",
                 project_date1: "2020_3분기",
+                href_2: "",
                 project_date2: "2020_4분기",
+                href_3: "",
                 project_date3: ""
             }
         ]
@@ -29,7 +33,9 @@ var projects = [
             {
                 href_1: "",
                 project_date1: "2020_3분기",
+                href_2: "",
                 project_date2: "2020_4분기",
+                href_3: "",
                 project_date3: "2021_1분기 ~ 2021_2분기"
             }
         ],
@@ -41,7 +47,9 @@ var projects = [
             {
                 href_1: "",
                 project_date1: "M_LAB",
+                href_2: "",
                 project_date2: "C_LAB",
+                href_3: "",
                 project_date3: ""
             }
         ],
@@ -53,12 +61,118 @@ for (var i in projects) {
     for (var j in project.info_data) {
         var date = project.info_data[j]
         document.getElementById("project_Box").innerHTML += `
-        <div id="contents_Box">
+        <div class="contents_Box">
             <div class="Btext">${project.project_name}</div>
             <div class="Stext"><a href="${date.href_1}">${date.project_date1}</a></div>
-            <div class="Stext"><a href="${project.href}">${date.project_date2}</a></div>
-            <div class="Stext"><a href="${project.href}">${date.project_date3}</a></div>
+            <div class="Stext"><a href="${date.href_2}">${date.project_date2}</a></div>
+            <div class="Stext"><a href="${date.href_3}">${date.project_date3}</a></div>
         </div>
         `;
     }
 }
+
+
+var project_info = [
+    {
+        id_name: "single_M3",
+        project_info_name: "사회적 거리두기로 인한 삼시세끼의 변화",
+        img_class_name: "image1",
+    },
+    {
+        id_name: "single_M4",
+        project_info_name: "사람들이 스타벅스에 미치는 이유",
+        img_class_name: "image2",
+    },
+    {
+        id_name: "subject_M3",
+        project_info_name: "천연 공기청정기;이끼",
+        img_class_name: "image3",
+    },
+    {
+        id_name: "subject_M4",
+        project_info_name: "지속가능한 삶을 위한 학교",
+        img_class_name: "image4",
+    },
+    {
+        id_name: "team_M3",
+        project_info_name: "우울한 청소년들",
+        img_class_name: "image5",
+    },
+    {
+        id_name: "team_M4",
+        project_info_name: "전통시장이 활성화되지 않는 것이 문제이다",
+        img_class_name: "image6s",
+    },
+    {
+        id_name: "team_M1M2",
+        project_info_name: "사회적 거리두기로 인한 삼시세끼의 변화",
+        img_class_name: "image7",
+    },
+    {
+        id_name: "alpha_M",
+        project_info_name: "사회적 거리두기로 인한 삼시세끼의 변화",
+        img_class_name: "image8",
+    },
+    {
+        id_name: "alpha_C",
+        project_info_name: "사회적 거리두기로 인한 삼시세끼의 변화",
+        img_class_name: "image9",
+    }
+]
+
+for (var i in project_info) {
+    var info = project_info[i]
+    document.getElementById("project_info_Box").innerHTML += `
+    <div class="project_info" id="${info.id_name}">
+        <div class="project_Name">
+            ${info.project_info_name}
+        </div>
+        <div id="${info.img_class_name}">
+        </div>
+    </div>
+    `;
+}
+
+
+// document.getElementById('single_M3')
+// document.getElementById('single_M4')
+// document.getElementById('subject_M3')
+// document.getElementById('subject_M4')
+// document.getElementById('team_M3')
+// document.getElementById('team_M4')
+// document.getElementById('team_M1M2')
+// document.getElementById('alpha_M')
+// document.getElementById('alpha_C')
+
+
+// function Stext_hover_1() {
+
+// }
+
+// function Stext_hover_2() {
+
+// }
+
+// function Stext_hover_3() {
+
+// }
+
+// function Stext_hover_4() {
+
+// }
+
+// function Stext_hover_5() {
+
+// }
+
+// function Stext_hover_6() {
+
+// }
+
+// function Stext_hover_7() {
+
+// }
+
+// function Stext_hover_8() {
+
+// }
