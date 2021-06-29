33457f34a7e033860d55d5b6f6658c7231298532
chan050316 <84690440+chan050316@users.noreply.github.com>
2021-06-29 14:00:53 +0900

    F 2021.6.29.2:00(PM)
    fixed img hover, img link

--- Project.js
+++ Project.js
@@ -115,46 +115,55 @@ var project_info = [
         id_name: "single_M3",
         project_info_name: "사회적 거리두기로 인한 삼시세끼의 변화",
         img_class_name: "image1",
+        img_link: "https://chan050316.github.io/roomme-portfolio/project_info_single1.html",
     },
     {
         id_name: "single_M4",
         project_info_name: "사람들이 스타벅스에 미치는 이유",
         img_class_name: "image2",
+        img_link: "https://chan050316.github.io/roomme-portfolio/project_info_single2.html",
     },
     {
         id_name: "subject_M3",
         project_info_name: "천연 공기청정기;이끼",
         img_class_name: "image3",
+        img_link: "https://chan050316.github.io/roomme-portfolio/project_info_subject1.html",
     },
     {
         id_name: "subject_M4",
         project_info_name: "지속가능한 삶을 위한 학교",
         img_class_name: "image4",
+        img_link: "https://chan050316.github.io/roomme-portfolio/project_info_subject2.html",
     },
     {
         id_name: "team_M3",
         project_info_name: "우울한 청소년들",
         img_class_name: "image5",
+        img_link: "https://chan050316.github.io/roomme-portfolio/project_info_team1.html",
     },
     {
         id_name: "team_M4",
         project_info_name: "전통시장이 활성화되지 않는 것이 문제이다",
         img_class_name: "image6",
+        img_link: "https://chan050316.github.io/roomme-portfolio/project_info_team2.html",
     },
     {
         id_name: "team_M1M2",
         project_info_name: "윈썸",
         img_class_name: "image7",
+        img_link: "https://chan050316.github.io/roomme-portfolio/project_info_team3.html",
     },
     {
         id_name: "alpha_M",
         project_info_name: "SSAP",
         img_class_name: "image8",
+        img_link: "https://chan050316.github.io/roomme-portfolio/project_info_alpha1.html",
     },
     {
         id_name: "alpha_C",
         project_info_name: "웹페이지 만들기",
         img_class_name: "image9",
+        img_link: "https://chan050316.github.io/roomme-portfolio/project_info_alpha2.html",
     }
 ]
 
@@ -165,7 +174,7 @@ for (var i in project_info) {
         <div class="project_Name">
             ${info.project_info_name}
         </div>
-        <div id="${info.img_class_name}" class="project_info_img">
+        <div id="${info.img_class_name}" class="project_info_img" OnClick="location.href='${info.img_link}'">
         </div>
     </div>
     `;
