a0d8154f5a41d373cfcfe4a7b1bac29ba7148425
chan050316 <84690440+chan050316@users.noreply.github.com>
2021-07-05 06:28:50 +0900

    C 2021.7.5.6:28(AM)
    create loading page

---
+++ index.js
@@ -0,0 +1,8 @@
+var a = document.querySelector('body');
+
+    window.onload = function () {
+      setTimeout(function () {
+        // a.classList.remove('hidden')
+        window.location.href = 'Main_page.html'
+      }, 5800);
+    }
