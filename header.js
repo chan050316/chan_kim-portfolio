bd4109174da698c0121c046eb38f74ac9c5a2447
chan050316 <84690440+chan050316@users.noreply.github.com>
2021-07-06 11:20:32 +0900

    M 2021.7.6.11:19(AM)
    media query
    
    - project_info[i]
    - main under the 'Bar'

--- header.js
+++ header.js
@@ -15,9 +15,15 @@ menu_list = [
 
 for (var i in menu_list) {
     var menu = menu_list[i];
-    document.getElementById('headerMenu_Box').innerHTML += `
+    document.getElementsByClassName('headerMenu_Box')[0].innerHTML += `
     <div class="headerMenu">
-        <a href="${menu.href}">${menu.Menu}</a>
+        <a href="${menu.href}" class="menus">${menu.Menu}</a>
     </div>
     `;
+}
+
+function menu_open() {
+    var menu = document.getElementsByClassName('headerMenu_Box')[0];
+
+    menu.classList.toggle('hidden')
 }
